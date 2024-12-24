46779abf26935095536219d512b5fec365b593f8
Meaghan Bruce <megleigh2462@gmail.com>
2024-12-24 03:17:42 -0500

    Add files via upload

---
+++ block_letters.py
@@ -0,0 +1,12 @@
+# My name is Meaghan Bruce and I have been a a data analyst for 10 years
+
+def print_m_block():
+    print("M   M  BBBB")
+    print("MM MM  B   B")
+    print("MM MM  B   B")
+    print("M M M  BBBB")
+    print("M   M  B   B")
+    print("M   M  B   B")
+    print("M   M  BBBB")
+
+print_m_block()
